a6fada311c86237ef0695016293a77daa60b5eae
Ismail Courr <contact@ismailcourr.ml>
2022-09-27 02:52:17 +0100

    Use Link in signin & signup components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Navigation from './components/Navigation';
 import Signin from './components/Signin';
 import Signup from './components/Signup';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navigation />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/signin" element={<Signin />} />
+        <Route exact path="/signup" element={<Signup />} />
         <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
     </BrowserRouter>
