eca48e5a2223e120be8702c4078eb282c4be9535
Ismail Courr <contact@ismailcourr.ml>
2022-10-02 00:30:00 +0100

    Fix overflow

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -13,12 +13,12 @@ function Navigation() {
     }
   };
   return (
-    <nav className="z-10 absolute top-0 h-20 w-screen flex justify-between items-center bg-white/50 shadow-2xl shadow-gray-100">
+    <nav className="z-10 absolute top-0 h-20 w-full flex justify-between items-center bg-white/50 shadow-2xl shadow-gray-100">
       <Link to="/">
         <img src={logo} alt="logo" className="logo h-16" />
       </Link>
-      <ul className={`sm:${isOpened ? '-translate-x-0' : '-translate-y-full translate-x-full'} duration-300 flex flex-col absolute h-screen bg-gray-800 lg:bg-gray-700/0 backdrop-blur-50 text-white lg:text-gray-900 p-32 lg:p-4 top-0 bottom-10 right-0 rounded-bl-full lg:rounded-none justify-start lg:justify-end items-start lg:flex-row lg:translate-x-0 z-10`}>
-        <li className="text-xl p-2 mx-1 my-1 lg:my-0  hover:bg-gray-600 lg:hover:bg-gray-200 rounded-md">
+      <ul className={`${isOpened ? 'translate-x-0' : 'translate-x-full'} duration-300 flex flex-col absolute h-screen w-full bg-gray-800 lg:bg-gray-700/0 backdrop-blur-50 text-white lg:text-gray-900 p-4 pl-32 pt-20 lg:pt-4 top-0 bottom-10 right-0 rounded-bl-full lg:rounded-none justify-start lg:justify-end items-start lg:flex-row lg:translate-x-0 z-10`}>
+        <li className="text-xl p-2 mx-1 my-1 lg:my-0 hover:bg-gray-600 lg:hover:bg-gray-200 rounded-md">
           <Link to="/features" onClick={() => toggleMenu(!isOpened)}>
             Features
           </Link>
@@ -44,7 +44,7 @@ function Navigation() {
           </Link>
         </li>
       </ul>
-      <button onClick={() => toggleMenu(!isOpened)} className="absolute m-4 right-0 flex flex-col items-center items-end justify-center w-10 h-10 bg-white/90 shadow shadow-xl shadow-gray-200 hover:shadow-inner rounded-full cursor-pointer lg:hidden hover:bg-gray-200 z-10" type="button">
+      <button onClick={() => toggleMenu(!isOpened)} className="absolute m-4 right-5 flex flex-col items-center items-end justify-center w-10 h-10 bg-white/90 shadow shadow-xl shadow-gray-200 hover:shadow-inner rounded-full cursor-pointer lg:hidden hover:bg-gray-200 z-10" type="button">
         <svg className={`${isOpened ? 'hidden' : ''} w-6 h-6 text-gray-700`} fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
           <path d="M4 6h16M4 12h16M4 18h16" className="" />
         </svg>
--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -8,10 +8,10 @@ function Signin() {
     alert('The form was submitted with the following data:');
   };
   return (
-    <div className="absolute top-0 bottom-0 left-0 right-0 flex flex-col justify-start items-center bg-loginBg bg-cover bg-center backdrop-opacity-10">
+    <div className="absolute h-full max-h-full top-0 bottom-0 left-0 right-0 flex flex-col justify-start items-center bg-loginBg bg-cover bg-repeat bg-center backdrop-opacity-10">
       <div className="backdrop-blur-md w-full h-full absolute" />
       <img className="w-40 m-4 mt-40 backdrop-opacity-0" src={logo} alt="logo" />
-      <section className="flex flex-col bg-gray-200/60 rounded-xl p-10 shadow-2xl backdrop-opacity-40">
+      <section className="flex flex-col bg-gray-200/60 rounded-xl p-5 shadow-2xl backdrop-opacity-40">
         <h1 className="font-bold text-2xl">Login to your account</h1>
         <form className="flex flex-col mt-4" onSubmit={(e) => handlSubmit(e)}>
           <input className="border-2 outline-none border-gray-300 rounded-full py-2 px-4 mt-2 bg-gray-100 invalid:border-b-red-300 valid:border-b-green-300" type="email" name="email" id="email" placeholder="user@email.com" required />
