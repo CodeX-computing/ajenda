110538862a6ece857811bcbe6937ccfb0168597c
Ismail Courr <contact@ismailcourr.ml>
2022-10-02 08:52:18 +0100

    Create base components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './components/About';
+import Features from './components/Features';
 import Home from './components/Home';
+import How from './components/How';
 import Navigation from './components/Navigation';
 import NotFound404 from './components/NotFound404';
 import Signin from './components/Signin';
@@ -12,6 +15,9 @@ function App() {
       <Navigation />
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/features" element={<Features />} />
+        <Route exact path="/how" element={<How />} />
+        <Route exact path="/about" element={<About />} />
         <Route exact path="/signin" element={<Signin />} />
         <Route exact path="/signup" element={<Signup />} />
         <Route path="*" element={<NotFound404 />} />
