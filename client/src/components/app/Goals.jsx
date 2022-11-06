b22505d0b2f57cf2c9709ed4066c28892a8d1399
Ismail Courr <contact@ismailcourr.ml>
2022-11-06 16:26:52 +0100

    Add over flow hidden, Add scrollbar-hide to todo & goals containers

--- client/src/components/app/Goals.jsx
+++ client/src/components/app/Goals.jsx
@@ -76,7 +76,7 @@ function Goals() {
 
   return (
     <div className="w-screen md:mb-16">
-      <div className="bg-gradient-to-b from-transparent to-gray-200 backdrop-blur-xl w-full h-full flex flex-col justify-start items-center py-32 lg:py-60 lg:pb-96 overflow-auto">
+      <div className="bg-gradient-to-b from-transparent to-gray-200 backdrop-blur-xl w-full h-full flex flex-col justify-start items-center py-32 lg:py-60 lg:pb-96 overflow-scroll scrollbar-hide">
         <div className="sm:w-6/6 md:w-5/6 bg-gray-500/50 p-4 rounded-xl shadow-xl shadow-gray-500/70 border border-gray-500">
           <h1 className="text-4xl font-bold m-4 hidden">Todo</h1>
           <div className="flex justify-between items-center">
--- client/src/components/app/Todo.jsx
+++ client/src/components/app/Todo.jsx
@@ -52,7 +52,7 @@ function Todo() {
 
   return (
     <div className="w-screen md:mb-16">
-      <div className="bg-gradient-to-b from-transparent to-gray-200 backdrop-blur-xl w-full h-full flex flex-col justify-start items-center py-32 lg:py-60 lg:pb-96">
+      <div className="overflow-scroll scrollbar-hide bg-gradient-to-b from-transparent to-gray-200 backdrop-blur-xl w-full h-full flex flex-col justify-start items-center py-32 lg:py-60 lg:pb-96">
         <div className="w-5/6 bg-gray-500/50 p-4 rounded-xl shadow-xl shadow-gray-500/70 border border-gray-500">
           <h1 className="text-4xl font-bold m-4 hidden">Todo</h1>
           <div className="flex justify-between items-center">
